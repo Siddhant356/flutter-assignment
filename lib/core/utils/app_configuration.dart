d01df881e77d9c62347ba5a6f61a4614095259f7
Siddhant Shukla <siddhants@goegonetwork.com.com>
2023-06-16 13:06:19 +0530

    calendar improvisation

--- lib/core/utils/app_configuration.dart
+++
@@ -1,7 +0,0 @@
-class AppConfiguration {
-  static const baseURL = String.fromEnvironment('baseUrl', defaultValue: 'https://api.kevee.com');
-  static const stripeSecret = String.fromEnvironment('stripeSecret', defaultValue: 'sk_test_51NG4EUSDadCoPzy5KhYnXTWWT326ozvkWnJb9wMQsYjEkYOdFCodPtWWizZy9oWRO4Kv1xhPHpH9P3DffOT3rayA00iXdgVPVb');
-  static const stripePublishableKey = String.fromEnvironment('stripePublishableKey', defaultValue: 'pk_test_51NG4EUSDadCoPzy52KDAD9OUCMitYVRjAhjrYCZ5VhUa1X6zx8zyQtcLQYMHHArN9I68uKgS2zokIH8kN7KFWaTw00ZpX0BX50');
-  static const googleMapKey = String.fromEnvironment('googleMapKey', defaultValue: 'AIzaSyAspPd5sXSPrJiBbCqzZ9ntP639ZrzPwGQ');
-  static const application = "kevee";
-}
